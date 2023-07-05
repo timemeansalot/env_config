a23173911372cadbc6c86fad3d8f811a3904ab5c
timemeansalot <timemeansalot@gmail.com>
2023-07-05 14:41:00 +0800

    do not work on linux

--- nvim/.config/nvim/lua/user/whichkey.lua
+++ nvim/.config/nvim/lua/user/whichkey.lua
@@ -122,10 +122,6 @@ local mappings = {
 		o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
 		b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
 		c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
-		d = {
-			"<cmd>Gitsigns diffthis HEAD<cr>",
-			"Diff",
-		},
 	},
 
 	l = {
