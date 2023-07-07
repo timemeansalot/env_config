6b5db55358aca45aeb1e50a304780aee404b3dea
timemeansalot <timemeansalot@gmail.com>
2023-07-07 23:46:48 +0800

    fix autolist new version bug

--- nvim/.config/nvim/lua/user/autolist.lua
+++ nvim/.config/nvim/lua/user/autolist.lua
@@ -1,38 +1,46 @@
---local status_ok, autolist= pcall(require, "autolist")
---if not status_ok then
---	return
---end
-----
----- autolist.setup()
----- autolist.create_mapping_hook("i", "<leader><cr>", autolist.new)
+-- local status_ok, autolist= pcall(require, "autolist")
+-- if not status_ok then
+-- return
+-- end
 --
---local status_ok, autolist = pcall(require, "autolist")
---if not status_ok then
---	return print("call autolist fail\n")
---end
---
---autolist.setup()
----- autolist.create_mapping_hook("i", "<CR>", autolist.new)
----- autolist.create_mapping_hook("i", "<Tab>", autolist.indent)
----- autolist.create_mapping_hook("i", "<S-Tab>", autolist.indent, "<C-D>")
---
---
----- very 卡，卡的很
----- autolist.create_mapping_hook("i", "<leader><CR>", autolist.new)
----- autolist.create_mapping_hook("i", "<leader><Tab>", autolist.indent)
----- autolist.create_mapping_hook("i", "<leader><S-Tab>", autolist.indent, "<C-D>")
---autolist.create_mapping_hook("n", "o", autolist.new)
---autolist.create_mapping_hook("n", "O", autolist.new_before)
----- autolist.create_mapping_hook("n", ">>", autolist.indent)
----- autolist.create_mapping_hook("n", "<<", autolist.indent)
---autolist.create_mapping_hook("n", "<leader>r", autolist.force_recalculate)
---autolist.create_mapping_hook("n", "<leader>x", autolist.invert_entry, "")
---vim.api.nvim_create_autocmd("TextChanged", {
---	pattern = "-",
---	callback = function()
---		vim.cmd.normal({ autolist.force_recalculate(nil, nil), bang = false })
---	end,
---})
+-- autolist.setup()
+-- autolist.create_mapping_hook("i", "<leader><cr>", autolist.new)
+
+local status_ok, autolist = pcall(require, "autolist")
+if not status_ok then
+return print("call autolist fail\n")
+end
+
+autolist.setup()
+vim.keymap.set("i", "<tab>", "<cmd>AutolistTab<cr>")
+vim.keymap.set("i", "<s-tab>", "<cmd>AutolistShiftTab<cr>")
+-- vim.keymap.set("i", "<c-t>", "<c-t><cmd>AutolistRecalculate<cr>") -- an example of using <c-t> to indent
+vim.keymap.set("i", "<CR>", "<CR><cmd>AutolistNewBullet<cr>")
+vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>")
+vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
+vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
+vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
+-- autolist.create_mapping_hook("i", "<CR>", autolist.new)
+-- autolist.create_mapping_hook("i", "<Tab>", autolist.indent)
+-- autolist.create_mapping_hook("i", "<S-Tab>", autolist.indent, "<C-D>")
+
+
+-- very 卡，卡的很
+-- autolist.create_mapping_hook("i", "<leader><CR>", autolist.new)
+-- autolist.create_mapping_hook("i", "<leader><Tab>", autolist.indent)
+-- autolist.create_mapping_hook("i", "<leader><S-Tab>", autolist.indent, "<C-D>")
+-- autolist.create_mapping_hook("n", "o", autolist.new)
+-- autolist.create_mapping_hook("n", "O", autolist.new_before)
+-- -- autolist.create_mapping_hook("n", ">>", autolist.indent)
+-- -- autolist.create_mapping_hook("n", "<<", autolist.indent)
+-- autolist.create_mapping_hook("n", "<leader>r", autolist.force_recalculate)
+-- autolist.create_mapping_hook("n", "<leader>x", autolist.invert_entry, "")
+-- vim.api.nvim_create_autocmd("TextChanged", {
+-- pattern = "-",
+-- callback = function()
+-- 	vim.cmd.normal({ autolist.force_recalculate(nil, nil), bang = false })
+-- end,
+-- })
 
 
 -- new config 
@@ -126,30 +134,30 @@
 
 
 
-config = function()
-    require("autolist").setup()
+-- config = function()
+--     require("autolist").setup()
 
-    vim.keymap.set("i", "<tab>", "<cmd>AutolistTab<cr>")
-    vim.keymap.set("i", "<s-tab>", "<cmd>AutolistShiftTab<cr>")
-    -- vim.keymap.set("i", "<c-t>", "<c-t><cmd>AutolistRecalculate<cr>") -- an example of using <c-t> to indent
-    vim.keymap.set("i", "<CR>", "<CR><cmd>AutolistNewBullet<cr>")
-    vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>")
-    vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
-    vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
-    vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
+--     vim.keymap.set("i", "<tab>", "<cmd>AutolistTab<cr>")
+--     vim.keymap.set("i", "<s-tab>", "<cmd>AutolistShiftTab<cr>")
+--     -- vim.keymap.set("i", "<c-t>", "<c-t><cmd>AutolistRecalculate<cr>") -- an example of using <c-t> to indent
+--     vim.keymap.set("i", "<CR>", "<CR><cmd>AutolistNewBullet<cr>")
+--     vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>")
+--     vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
+--     vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
+--     vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
 
-    -- cycle list types with dot-repeat
-    vim.keymap.set("n", "<leader>cn", require("autolist").cycle_next_dr, { expr = true })
-    vim.keymap.set("n", "<leader>cp", require("autolist").cycle_prev_dr, { expr = true })
+--     -- cycle list types with dot-repeat
+--     vim.keymap.set("n", "<leader>cn", require("autolist").cycle_next_dr, { expr = true })
+--     vim.keymap.set("n", "<leader>cp", require("autolist").cycle_prev_dr, { expr = true })
 
-    -- if you don't want dot-repeat
-    -- vim.keymap.set("n", "<leader>cn", "<cmd>AutolistCycleNext<cr>")
-    -- vim.keymap.set("n", "<leader>cp", "<cmd>AutolistCycleNext<cr>")
+--     -- if you don't want dot-repeat
+--     -- vim.keymap.set("n", "<leader>cn", "<cmd>AutolistCycleNext<cr>")
+--     -- vim.keymap.set("n", "<leader>cp", "<cmd>AutolistCycleNext<cr>")
 
-    -- functions to recalculate list on edit
-    vim.keymap.set("n", ">>", ">><cmd>AutolistRecalculate<cr>")
-    vim.keymap.set("n", "<<", "<<<cmd>AutolistRecalculate<cr>")
-    vim.keymap.set("n", "dd", "dd<cmd>AutolistRecalculate<cr>")
-    vim.keymap.set("v", "d", "d<cmd>AutolistRecalculate<cr>")
-end
+--     -- functions to recalculate list on edit
+--     vim.keymap.set("n", ">>", ">><cmd>AutolistRecalculate<cr>")
+--     vim.keymap.set("n", "<<", "<<<cmd>AutolistRecalculate<cr>")
+--     vim.keymap.set("n", "dd", "dd<cmd>AutolistRecalculate<cr>")
+--     vim.keymap.set("v", "d", "d<cmd>AutolistRecalculate<cr>")
+-- end
 
