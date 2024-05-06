ead406af93acb681a3e02655b8ccbf715810d8fb
timemeansalot <timemeansalot@gmail.com>
2024-05-06 10:14:59 +0800

    finish copy config file from josean

---
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,58 @@
+return {
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    -- import mason
+    local mason = require("mason")
+
+    -- import mason-lspconfig
+    local mason_lspconfig = require("mason-lspconfig")
+
+    local mason_tool_installer = require("mason-tool-installer")
+
+    -- enable mason and configure icons
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
+
+    mason_lspconfig.setup({
+      -- list of servers for mason to install
+      ensure_installed = {
+        "lua_ls",
+        "pyright",
+        "gopls",
+      },
+    })
+
+    mason_tool_installer.setup({
+      ensure_installed = {
+        -- lua related tools
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+        "isort", -- python formatter
+        "black", -- python formatter
+        "pylint",
+        "eslint_d",
+        "shfmt",
+        "lua-language-server",
+        -- python related tools
+        "black",
+        "debugpy",
+        "mypy",
+        "ruff-lsp",
+        "pyright",
+        -- golang related tools
+        "gopls",
+      },
+    })
+  end,
+}
