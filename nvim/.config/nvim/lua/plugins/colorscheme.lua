38fe29aa16913b9414400d443c152aecc7facb47
timemeansalot <timemeansalot@gmail.com>
2024-07-17 14:56:15 +0000

    fix conflic

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -69,7 +69,7 @@ local catppuccin = {
   },
   config = function()
     -- vim.cmd([[colorscheme catppuccin-latte]]) -- use vim.cmd api to use this colorscheme
-    vim.cmd([[colorscheme catppuccin-mocha]]) -- use vim.cmd api to use this colorscheme
+    vim.cmd([[colorscheme tokyonight]]) -- use vim.cmd api to use this colorscheme
   end,
 }
 -- return gruvbox
