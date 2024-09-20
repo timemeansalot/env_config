0bc69a644d2f9f7c5c64b057a6c47d81e4e5b349
timemeansalot <timemeansalot@gmail.com>
2024-09-20 16:59:07 +0800

    update on lenovo

--- nvim_mine/.config/nvim/lua/plugins/ui.lua
+++ nvim_mine/.config/nvim/lua/plugins/ui.lua
@@ -193,7 +193,7 @@ return {
           untracked    = { text = '┆' },
         },
         signs_staged_enable          = true,
-        signcolumn                   = true, -- Toggle with `:Gitsigns toggle_signs`
+        signcolumn                   = true,  -- Toggle with `:Gitsigns toggle_signs`
         numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
         linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
         word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
@@ -214,7 +214,7 @@ return {
         current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
         sign_priority                = 6,
         update_debounce              = 100,
-        status_formatter             = nil, -- Use default
+        status_formatter             = nil,   -- Use default
         max_file_length              = 40000, -- Disable if file is longer than this (in lines)
         preview_config               = {
           -- Options passed to nvim_open_win
@@ -226,5 +226,14 @@ return {
         },
       }
     end
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
   },
 }
