d10dce63c6adf3429c3b29edf3c2f3159940b679
timemeansalot <timemeansalot@gmail.com>
2024-09-27 14:11:21 +0800

    modify on lenovo

--- nvim_mine/.config/nvim/lua/plugins/ui.lua
+++ nvim_mine/.config/nvim/lua/plugins/ui.lua
@@ -224,6 +224,45 @@ return {
           row = 0,
           col = 1
         },
+        on_attach                    = function(bufnr)
+          local gitsigns = require('gitsigns')
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation
+          map('n', ']c', function()
+            if vim.wo.diff then
+              vim.cmd.normal({ ']c', bang = true })
+            else
+              gitsigns.nav_hunk('next')
+            end
+          end)
+
+          map('n', '[c', function()
+            if vim.wo.diff then
+              vim.cmd.normal({ '[c', bang = true })
+            else
+              gitsigns.nav_hunk('prev')
+            end
+          end)
+
+          -- Actions
+          map('n', '<leader>gs', gitsigns.stage_hunk, { desc = "stage_hunk" })
+          map('n', '<leader>gr', gitsigns.reset_hunk, { desc = "reset_hunk" })
+          map('v', '<leader>gs', function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+          map('v', '<leader>gr', function() gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+          map('n', '<leader>gS', gitsigns.stage_buffer, { desc = "stage_buffer" })
+          map('n', '<leader>gu', gitsigns.undo_stage_hunk, { desc = "undo_stage_hunk" })
+          map('n', '<leader>gR', gitsigns.reset_buffer, { desc = "reset_buffer" })
+          map('n', '<leader>gb', function() gitsigns.blame_line { full = true } end, { desc = "blame_line" })
+          map('n', '<leader>gb', gitsigns.toggle_current_line_blame, { desc = "toggle_current_line_blame" })
+          map('n', '<leader>gd', gitsigns.diffthis, { desc = "diffthis" })
+          map('n', '<leader>gD', function() gitsigns.diffthis('~') end, { desc = "diff head~" })
+        end
       }
     end
   },
