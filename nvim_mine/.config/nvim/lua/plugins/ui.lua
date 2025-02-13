d40927afc6ed457264bc8a7271c07d911644e3b0
timemeansalot <timemeansalot@gmail.com>
2025-02-13 19:40:51 +0800

    add more ui plugins
    added following ui plugins:
    - popup ui when type vim commands
    - colorful brackets and lines

--- nvim_mine/.config/nvim/lua/plugins/ui.lua
+++ nvim_mine/.config/nvim/lua/plugins/ui.lua
@@ -63,7 +63,7 @@ return {
       { "<leader>bo", "<cmd>BufferLineCloseOthers<cr>", desc = "close other buffers" },
       { "<leader>bl", "<cmd>BufferLineCloseLeft<cr>",   desc = "close all left buffers" },
       { "<leader>br", "<cmd>BufferLineCloseRight<cr>",  desc = "close all right buffers" },
-      { "<leader>x",  "<cmd>bd<cr>",                    desc = "close current buffers" },
+      { "<leader>bx", "<cmd>bd<cr>",                    desc = "close current buffers" },
     }
   },
   {
@@ -282,4 +282,55 @@ return {
     end,
     ft = { "markdown" },
   },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+  },
+
+
+   {
+    -- colorful indents line
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      local highlight = {
+        "RainbowRed",
+        "RainbowYellow",
+        "RainbowBlue",
+        "RainbowOrange",
+        "RainbowGreen",
+        "RainbowViolet",
+        "RainbowCyan",
+      }
+      local hooks = require("ibl.hooks")
+      -- create the highlight groups in the highlight setup hook, so they are reset
+      -- every time the colorscheme changes
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+      end)
+
+      vim.g.rainbow_delimiters = { highlight = highlight }
+      require("ibl").setup({ scope = { highlight = highlight } })
+
+      hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+    end,
+  },
+  { "HiPhish/rainbow-delimiters.nvim" }, -- colorful brackets
+
 }
