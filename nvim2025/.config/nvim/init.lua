c048b37c887e233f2e4f0a0bd0f35614c8c4ba51
timemeansalot <timemeansalot@gmail.com>
2025-10-20 19:34:07 +0800

    modify the nvim to use light theme

--- nvim2025/.config/nvim/init.lua
+++ nvim2025/.config/nvim/init.lua
@@ -860,7 +860,7 @@ require('lazy').setup({
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      vim.cmd.colorscheme 'tokyonight-day'
+      vim.cmd.colorscheme 'tokyonight'
       vim.cmd.hi 'Comment gui=none'
     end,
   },
