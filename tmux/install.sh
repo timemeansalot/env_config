34f058f00991c2a6f728984891de935723a26be6
timemeansalot <timemeansalot@gmail.com>
2026-04-03 07:54:56 +0000

    Improve tmux install and shell portability

--- tmux/install.sh
+++ tmux/install.sh
@@ -4,3 +4,7 @@ set -euo pipefail
 
 sudo apt-get update
 sudo apt-get install -y tmux zsh git
+
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
